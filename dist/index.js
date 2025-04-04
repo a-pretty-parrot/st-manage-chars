5f261de949e3a243b5726c7b7ba33ffce7992d03
a pretty parrot <aburse@gmail.com>
2025-04-04 13:56:00 -0400

    init

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,22 @@
 console.log('[nav-button] >>> MINIMAL DIST INDEX LOADED <<<');
 
-registerExtension({
-    name: 'st-manage-chars',
-    init(context) {
-        console.log('[nav-button] init() started');
-        context.onEnable(() => {
-            console.log('[nav-button] onEnable() fired');
+function initWhenReady() {
+    if (typeof registerExtension === 'function') {
+        console.log('[nav-button] registerExtension is available');
+
+        registerExtension({
+            name: 'st-manage-chars',
+            init(context) {
+                console.log('[nav-button] init() started');
+                context.onEnable(() => {
+                    console.log('[nav-button] onEnable() fired');
+                });
+            }
         });
+    } else {
+        console.log('[nav-button] registerExtension not ready yet, retrying...');
+        setTimeout(initWhenReady, 250);
     }
-});
+}
+
+initWhenReady();
